59e3d731ab45ff04bd281f9099e8a8b3b05748da
Alexandru Rădulescu <alex11amr110506@gmail.com>
2025-11-22 14:51:13 +0200

    implement hoc for managing windows and added terminal window

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import NavBar from '#components/NavBar'
 import Welcome from '#components/Welcome'
 import Dock from '#components/Dock'
 import React from 'react'
+import gsap from 'gsap'
+import Draggable from 'gsap/Draggable'
+import Terminal from '#windows/Terminal'
+gsap.registerPlugin(Draggable)
 
 const App = () => {
   return (
@@ -9,6 +13,8 @@ const App = () => {
       <NavBar/>
       <Welcome/>
       <Dock/>
+
+      <Terminal/>
     </main>
   )
 }
